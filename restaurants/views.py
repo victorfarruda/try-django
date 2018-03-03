52478e77a9259ddc26459eeb82078cfed704c82f
Victor de Freitas Arruda <victorf_arruda@outlook.com>
2018-03-03 18:20:15 -0300

    19

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,8 +1,8 @@
 from django.db.models import Q
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 
 from .models import RestaurantLocation
 
@@ -25,3 +25,6 @@ class RestaurantListView(ListView):
         else:
             queryset = RestaurantLocation.objects.all()
         return queryset
+
+class RestaurantDetailView(DetailView):
+    queryset = RestaurantLocation.objects.all()
