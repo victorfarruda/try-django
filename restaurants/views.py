843e2dadce1f79a0d58ab742e255302c7dc7c5e2
Victor de Freitas Arruda <victorf_arruda@outlook.com>
2018-03-03 17:20:05 -0300

    18

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,14 +1,27 @@
+from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 from .models import RestaurantLocation
 
 def restaurant_listview(request):
-    template_name = 'restaurant/restaurant_list.html'
+    template_name = 'restaurants/restaurantlocation_list.html.html'
     queryset = RestaurantLocation.objects.all()
     context = {
         "object_list": queryset
     }
     return render(request, template_name, context)
+
+class RestaurantListView(ListView):
+    def get_queryset(self):
+        slug = self.kwargs.get('slug')
+        if slug:
+            queryset = RestaurantLocation.objects.filter(
+                Q(category__iexact = slug)|
+                Q(category__icontains = slug)
+            )
+        else:
+            queryset = RestaurantLocation.objects.all()
+        return queryset
