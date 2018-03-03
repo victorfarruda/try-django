f9bc484cc4590465be63fc11a052f6492f765299
Victor de Freitas Arruda <victorf_arruda@outlook.com>
2018-03-03 19:06:52 -0300

    22

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import pre_save, post_save
+
+from .utils import unique_slug_generator
 
 class RestaurantLocation(models.Model):
     name          = models.CharField(max_length = 120)
@@ -15,3 +18,16 @@ class RestaurantLocation(models.Model):
     @property
     def title(self):
         return self.name
+
+def rl_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+# def rl_post_save_receiver(sender, instance, *args, **kwargs):
+#     print('saved')
+#     print(instance.timestamp)
+#     if not instance.slug:
+#     instance.slug = unique_slug_generator(instance)
+
+pre_save.connect(rl_pre_save_receiver, sender = RestaurantLocation)
+# post_save.connect(rl_post_save_receiver, sender = RestaurantLocation)
