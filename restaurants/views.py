097df06f36bdeda82c5862aa5c2fac1c84eb2266
Victor de Freitas Arruda <victorf_arruda@outlook.com>
2018-03-04 23:15:50 -0300

    30

--- restaurants/views.py
+++ restaurants/views.py
@@ -66,3 +66,8 @@ class RestaurantCreateView(CreateView):
     form_class = RestaurantLocationCreateForm
     template_name = 'restaurants/form.html'
     success_url = '/restaurants/'
+
+    def form_valid(self, form):
+        instance = form.save(commit = False)
+        instance.owner = self.request.user
+        return super(RestaurantCreateView, self).form_valid(form)
