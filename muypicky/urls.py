490a01633355e993c8537e6a36705d1d75841722
Victor de Freitas Arruda <victorf_arruda@outlook.com>
2018-03-05 00:59:20 -0300

    32

--- muypicky/urls.py
+++ muypicky/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from django.contrib.auth.views import LoginView
+
 from django.views.generic import TemplateView
 from restaurants.views import restaurant_listview, RestaurantListView, RestaurantDetailView, restaurant_createview, RestaurantCreateView
 
@@ -23,6 +25,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name = 'home.html')),
     url(r'^about/$', TemplateView.as_view(template_name = 'about.html')),
+    url(r'^login/$', LoginView.as_view(), name = 'login'),
     url(r'^restaurants/$', RestaurantListView.as_view()),
     url(r'^restaurants/create/$', RestaurantCreateView.as_view()),
     #url(r'^restaurants/(?P<slug>\w+)/$', RestaurantListView.as_view()),
