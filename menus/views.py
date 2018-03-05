15df443d05306d5a749a8bae00f2a1d8b6e7a283
Victor de Freitas Arruda <victorf_arruda@outlook.com>
2018-03-05 14:07:43 -0300

    35

--- menus/views.py
+++ menus/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
-# Create your views here.
+from .models import Item
+from .forms import ItemForm
+
+class ItemListView(ListView):
+    def get_queryset(self):
+        return Item.objects.filter(user=self.request.user)
+
+class ItemDetailView(DetailView):
+    def get_queryset(self):
+        return Item.objects.filter(user=self.request.user)
+
+class ItemCreateView(CreateView):
+    form_class = ItemForm
+    template_name = 'form.html'
+
+    def form_valid(self, form):
+        instance = form.save(commit = False)
+        instance.user = self.request.user
+        return super(ItemCreateView, self).form_valid(form)
+
+    def get_queryset(self):
+        return Item.objects.filter(user=self.request.user)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ItemCreateView, self).get_context_data(*args, **kwargs)
+        context['title'] = 'Add Item'
+        return context
+
+class ItemUpdateView(UpdateView):
+    template_name = 'form.html'
+    form_class = ItemForm
+    def get_queryset(self):
+        return Item.objects.filter(user=self.request.user)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ItemUpdateView, self).get_context_data(*args, **kwargs)
+        context['title'] = 'Update Item'
+        return context
--- muypicky/urls.py
+++ muypicky/urls.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView
 
 from django.views.generic import TemplateView
-from restaurants.views import restaurant_listview, RestaurantListView, RestaurantDetailView, restaurant_createview, RestaurantCreateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -27,5 +26,6 @@ urlpatterns = [
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name="about"),
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^restaurants/', include('restaurants.urls', namespace='restaurants')),
+    url(r'^items/', include('menus.urls', namespace='menus')),
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name="contact"),
 ]
