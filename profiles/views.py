f689b9fd22e4d85283318acbd30c9802197584f5
Victor de Freitas Arruda <victorf_arruda@outlook.com>
2018-03-05 21:42:42 -0300

    40

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,9 @@ from django.http import Http404
 from django.views.generic import DetailView
 from django.contrib.auth import get_user_model
 
+from restaurants.models import RestaurantLocation
+from menus.models import Item
+
 User = get_user_model()
 
 class ProfileDetailView(DetailView):
@@ -13,3 +16,13 @@ class ProfileDetailView(DetailView):
         if username is None:
             raise Http404
         return get_object_or_404(User, username__iexact=username, is_active=True)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProfileDetailView, self).get_context_data(*args, **kwargs)
+        user = context['user']
+        query = self.request.GET.get('q')
+        item_exists = Item.objects.filter(user=user).exists()
+        qs = RestaurantLocation.objects.filter(owner=user).search(query)
+        if item_exists and qs.exists():
+            context['locations'] = qs
+        return context
