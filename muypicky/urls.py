3b5bef73c128181bc816bd6e024c332339c0258d
Victor de Freitas Arruda <victorf_arruda@outlook.com>
2018-03-07 10:37:37 -0300

    42

--- muypicky/urls.py
+++ muypicky/urls.py
@@ -19,12 +19,14 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView
 
 from django.views.generic import TemplateView
+from profiles.views import ProfileFollowToggle
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='home.html'), name="home"),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name="about"),
     url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^profile-follow/',ProfileFollowToggle.as_view(), name='follow'),
     url(r'^restaurants/', include('restaurants.urls', namespace='restaurants')),
     url(r'^items/', include('menus.urls', namespace='menus')),
     url(r'^u/', include('profiles.urls', namespace='profile')),
